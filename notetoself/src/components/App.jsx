bc7d24e1012b3277a9c46984beb89deba275dfd1
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-16 15:33:08 -0400

    refactor shorthand destructuring

--- notetoself/src/components/App.jsx
+++ notetoself/src/components/App.jsx
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      notes: []
+    };
+  }
+
+  submit() {
+    const { notes, text } = this.state;
+    notes.push({ text });
+    this.setState({ notes });
+  }
+
   render() {
     return (
       <div>
         <h2>Note to Self</h2>
-        <Form>
-          <FormControl/>
-          <Button>Sumbit</Button>
+        <Form inline>
+          <FormControl
+            onChange={e => this.setState({ text: e.target.value })}
+          />
+          <Button onClick={() => this.submit()}>Submit</Button>
         </Form>
+        {this.state.notes.map((note, index) => {
+          return <div key={index}>{note.text}</div>;
+        })}
       </div>
     );
   }
