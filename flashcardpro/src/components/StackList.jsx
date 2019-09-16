408e719380e0ecf4fadf40d45b48fd8eba223e28
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-16 18:50:48 -0400

    setup Link routes

--- flashcardpro/src/components/StackList.jsx
+++ flashcardpro/src/components/StackList.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import stacks from '../data/stacks.json';
+import { Link } from 'react-router-dom';
 
 export default class StackList extends Component {
   render() {
     return (
       <div>
         {stacks.map(stack => {
-          return <h4 key={stack.id}>{stack.title}</h4>;
+          return (
+            <Link to="/stack" key={stack.id}>
+              <h4>{stack.title}</h4>
+            </Link>
+          );
         })}
       </div>
     );
--- flashcardpro/src/index.jsx
+++ flashcardpro/src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Stack from './components/Stack';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={App} />
+      <Route path='/stack' component={Stack} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
