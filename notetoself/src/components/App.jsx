02e4b2dcbc13ae5718a5a56399969a22cc863ca6
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-16 15:44:37 -0400

    add style to Note component

--- notetoself/src/components/App.jsx
+++ notetoself/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
+import Note from './Note';
 
 class App extends Component {
   constructor(props) {
@@ -26,9 +27,9 @@ class App extends Component {
           />
           <Button onClick={() => this.submit()}>Submit</Button>
         </Form>
-        {this.state.notes.map((note, index) => {
-          return <div key={index}>{note.text}</div>;
-        })}
+        {this.state.notes.map((note, index) => (
+          <Note key={index} note={note} />
+        ))}
       </div>
     );
   }
